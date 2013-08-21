0195a3614982244a6722ff09a0f7140bb4da219b
Tom Hanoldt <tom.hanoldt@swe-01.kp.de>
2013-08-21 14:11:58 +0200

    shared/tools/* added in link task

--- config/common/tasks.rb
+++ config/common/tasks.rb
@@ -11,13 +11,13 @@ namespace :deploy do
   task :finalize_update, :roles => :app do
   end
 
-  task :symlink_config_folder, :roles => :app do
+  task :symlink_shared_folders, :roles => :app do
     run <<-CMD
-      rm -Rf #{release_path}/conf && ln -s #{shared_path}/conf #{release_path}/conf && ln -s #{shared_path}/data #{release_path}/data
+      rm -Rf #{release_path}/conf && ln -s #{shared_path}/conf #{release_path}/conf && ln -s #{shared_path}/data #{release_path}/data && ln -s #{shared_path}/tools #{release_path}/tools
     CMD
   end
 end
 
 
-after "deploy",         "deploy:cleanup"
-after "deploy:create_symlink",         "deploy:symlink_config_folder"
+after "deploy", "deploy:cleanup"
+after "deploy:create_symlink", "deploy:symlink_shared_folders"
