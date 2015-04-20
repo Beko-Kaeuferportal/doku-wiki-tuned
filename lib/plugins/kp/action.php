19d837f121cf481d6aa128aae4ad51090421fedc
tom.hanoldt <tom.hanoldt@kaeuferportal.de>
2015-04-20 09:17:31 +0200

    real ip

--- lib/plugins/kp/action.php
+++ lib/plugins/kp/action.php
@@ -53,6 +53,9 @@ class action_plugin_kp extends DokuWiki_Action_Plugin {
     if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
       return $_SERVER['HTTP_X_FORWARDED_FOR'];
 
+    if(isset($_SERVER['HTTP_CF_CONNECTING_IP']))
+      return $_SERVER['HTTP_CF_CONNECTING_IP'];
+
     return $_SERVER['REMOTE_ADDR'];
   }
 
