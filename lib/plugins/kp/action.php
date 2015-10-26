c1fdbca8ce90544f06a4f75cb59f6e8e6dddf782
Stefan Schmidt <stefan.schmidt@kaeuferportal.de>
2015-10-26 18:20:54 +0100

    Hide sidebar form externals

--- lib/plugins/kp/action.php
+++ lib/plugins/kp/action.php
@@ -8,7 +8,7 @@
 
 if(!defined('DOKU_INC')) die();
 
-class eeeeeeeplugin_kp extends DokuWiki_Action_Plugin {
+class action_plugin_kp extends DokuWiki_Action_Plugin {
   static $PUBLIC_WIKI_ID_MATCHER  = '/.*id=support.*/i';
   static $PUBLIC_WIKI_ALLOWED_IPS = array('217.110.41.43', '212.202.220.8', '83.236.161.184', '127.0.0.1');
 
